7dbbe2f58e76af47a0039d46dfc47419679b3f20
anto-muli <memuli@students@uonbi.ac.ke>
2023-10-06 16:37:38 +0300

    shell

---
+++ histo_am.c
@@ -0,0 +1,155 @@
+#include "shell.h"
+
+/**
+ * addtoHistoryList - Add an entry to a history linked list.
+ * @info: A structure containing relevant arguments.
+ * Used to maintain state.
+ *
+ * @buf: The string to add to the history list.
+ * @lineCount: The current history line count (histcount)
+ *
+ * Return: Always returns 0.
+ */
+int addtoHistoryList(info_t *info, char *buf, int lineCount)
+{
+	list_t *newNode = NULL;
+
+	if (info->history)
+		newNode = info->history;
+
+	addNodeAtEnd(&newNode, buf, lineCount);
+
+	if (!info->history)
+		info->history = newNode;
+	return (0);
+}
+
+/**
+  * loadHistoryFromFile - Read and load history data from a file.
+  * @info: The parameter struct containing relevant information.
+  *
+  * Return: Returns the number of history entries read on success,
+  * or 0 otherwise.
+  */
+int loadHistoryFromFile(info_t *info)
+{
+	int x, last = 0, lineCount = 0;
+	ssize_t fd, readLength, fileSize = 0;
+	struct stat fileStat;
+	char *buf = NULL, *filePath = fetchHistoryFilePath(info);
+
+	if (!filePath)
+		return (0);
+	fd = open(filePath, O_RDONLY);
+	free(filePath);
+	if (fd == -1)
+		return (0);
+	if (!fstat(fd, &fileStat))
+		fileSize = fileStat.st_size;
+	if (fileSize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (fileSize + 1));
+	if (!buf)
+		return (0);
+	readLength = read(fd, buf, fileSize);
+	buf[fileSize] = 0;
+	if (readLength <= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (x = 0; x < fileSize; x++)
+	{
+		if (buf[x] == '\n')
+		{
+			buf[x] = 0;
+			addtoHistoryList(info, buf + last, lineCount++);
+			last = x + 1;
+		}
+	}
+	if (last != x)
+		addtoHistoryList(info, buf + last, lineCount++);
+	free(buf);
+	info->histcount = lineCount;
+	while (info->histcount-- >= MAX_HISTORY)
+		deleteNodeAtIndex(&(info->history), 0);
+	updateHistoryNumbers(info);
+	return (info->histcount);
+}
+
+/**
+ * writeHistoryToFile -  data to a file, creating it if
+ * necessary or overwriting it.
+ * @info: The parameter struct containing relevant information.
+ *
+ * Return: Returns 1 on success, or -1 on failure.
+ */
+int writeHistoryToFile(info_t *info)
+{
+	ssize_t fd;
+	char *filePath = fetchHistoryFilePath(info);
+	list_t *CN = NULL;
+
+	if (!filePath)
+		return (-1);
+
+	fd = open(filePath, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filePath);
+
+	if (fd == -1)
+		return (-1);
+
+	for (CN = info->history; CN; CN = CN->nextNode)
+	{
+		_putsfd(CN->stringValue, fd);
+		_putfd('\n', fd);
+	}
+	_putfd(FLUSH_BUFFER, fd);
+	close(fd);
+	return (1);
+}
+
+/**
+ * fetchHistoryFilePath - Retrieve the history file path.
+ * @info: A struct containing relevant information.
+ *
+ * Return: A dynamically allocated string containing
+ * the path to the history file.
+ */
+char *fetchHistoryFilePath(info_t *info)
+{
+	char *buf, *direct;
+
+	direct = _retrieveEnvironmentValue(info, "HOME=");
+	if (!direct)
+		return (NULL);
+
+	buf = malloc(sizeof(char) * (strlen(direct) + strlen(HIST_FILE) + 2));
+	if (!buf)
+		return (NULL);
+
+	buf[0] = 0;
+	_strcpy(buf, direct);
+	concatenate_strings(buf, "/");
+	concatenate_strings(buf, HIST_FILE);
+	return (buf);
+}
+
+/**
+ * updateHistoryNumbers -e the numbering of history
+ * entries of linked list after changes.
+ * @info: A structure containing relevant arguments.
+ * Used for maintenance.
+ *
+ * Return: The new history entry count (histcount).
+ */
+int updateHistoryNumbers(info_t *info)
+{
+	list_t *currentNode = info->history;
+	int newHistCount = 0;
+
+	while (currentNode)
+	{
+		currentNode->num = newHistCount++;
+		currentNode = currentNode->nextNode;
+	}
+	return (info->histcount = newHistCount);
+}
---
+++ loop_am.c
@@ -0,0 +1,194 @@
+#include "shell.h"
+
+/**
+ * main_shell_loop - Main function for the shell program.
+ *
+ * @info: Pointer to a structure containing information about the shell.
+ * @argv:   Argument vector from the main() function.
+ *
+ * Return: 0 on success, 1 on error, or an error code.
+ */
+int main_shell_loop(info_t *info, char **argv)
+{
+	ssize_t r = 0;
+	int built_in_result = 0;
+
+	while (r != -1 && built_in_result != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(FLUSH_BUFFER);
+		r = get_input(info);
+		if (r != -1)
+		{
+			set_info(info, argv);
+			built_in_result = find_and_execute_builtin(info);
+			if (built_in_result == -1)
+				find_and_execute_builtin(info);
+		}
+		else if (interactive(info))
+		{
+			_putchar('\n');
+		}
+		free_info(info, 0);
+	}
+	writeHistoryToFile(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (built_in_result == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (built_in_result);
+}
+
+
+/**
+ * find_and_execute_builtin - Find and execute a built-in command.
+ *
+ * @info: Pointer to a structure containing information about the shell.
+ *
+ * Return: -1 if the built-in command is not found,
+ * 0 if the built-in command is executed successfully,
+ * 1 if the built-in command is found but not successful,
+ * -2 if the built-in command signals an exit().
+ */
+int find_and_execute_builtin(info_t *info)
+{
+	struct builtin_table
+	{
+		const char *name;
+		int (*func)(info_t *);
+	};
+
+	int _myexit(info_t *info);
+	int _printEnvironment(info_t *info);
+	int _myhelp(info_t *info);
+	int _myhistory(info_t *info);
+	int _mysetenv(info_t *info);
+	int _myunsetenv(info_t *info);
+	int _mycd(info_t *info);
+	int _myalias(info_t *info);
+
+	struct builtin_table builtintbl[] = {
+		{"exit", _myexit},
+		{"env", _printEnvironment},
+		{"help", _myhelp},
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _mycd},
+		{"alias", _myalias},
+	};
+	int y, builtin_result = -1;
+
+	for (y = 0; builtintbl[y].name; y++)
+	{
+		if (strcmp(info->argv[0], builtintbl[y].name) == 0)
+		{
+			info->line_count++;
+			builtin_result = builtintbl[y].func(info);
+			break;
+		}
+	}
+	return (builtin_result);
+}
+
+/**
+ * find_and_execute_command - Find and execute a command in the PATH.
+ *
+ * @info: Pointer to a structure containing information about the shell.
+ *
+ * Return: void
+ */
+void find_and_execute_command(info_t *info)
+{
+	char *path = NULL;
+	int a, j;
+
+	info->path = info->argv[0];
+
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
+	for (a = 0; info->argument[a]; a++)
+	{
+		j = 0;
+	}
+	if (!is_delim(info->argument[a], " \t\n"))
+		j++;
+	if (!j)
+		return;
+	path = findCommandPath(info, _retrieveEnvironmentValue(info, "PATH="),
+			info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_and_execute_command(info);
+	}
+	else
+	{
+		if ((interactive(info) || _retrieveEnvironmentValue(info, "PATH=")
+					|| info->argv[0][0] == '/') && isExecutableCommand(info, info->argv[0]))
+			fork_and_execute_command(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "not found\n");
+		}
+	}
+}
+
+/**
+ * fork_and_execute_command - Forks a new process to execute a
+ * command using execve.
+ *
+ * @info: Pointer to a structure containing information about the shell.
+ *
+ * Return: void
+ */
+void fork_and_execute_command(info_t *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		/* TODO: PUT ERROR FUNCTION */
+		perror("Fork error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+			{
+				exit(126);
+			}
+			exit(1);
+		}
+		/* TODO: PUT ERROR FUNCTION */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+
+			if (info->status == 126)
+			{
+				print_error(info, "Permission denied\n");
+			}
+		}
+	}
+}
